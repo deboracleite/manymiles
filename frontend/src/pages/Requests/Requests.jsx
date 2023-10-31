e44fc36a4f4d64b55e809bf30a38dd391eca5360
Samarth Patel <65705790+Samarth21101999@users.noreply.github.com>
2023-10-31 02:41:06 -0400

    Header added in requests page

--- frontend/src/pages/Requests/Requests.jsx
+++ frontend/src/pages/Requests/Requests.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import api from '../../services/api'
 import RequestsList from '../Requests/RequestsList'
 import { useEffect } from 'react';
+import Header from '../../components/header/Header';
 const Requests = () => {
   const[requests, setRequests]= useState([]);
   const [checkRequest,setCheckRequest]=useState(false);
@@ -20,7 +21,7 @@ useEffect(()=> {
       
     <div>
 
-      
+      <Header/>
       {requests.map(request => (<RequestsList key={request.id} className="requests" {...request}/>))}
       
     </div>
--- frontend/src/pages/Requests/RequestsList.jsx
+++ frontend/src/pages/Requests/RequestsList.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Header from '../../components/header/Header'
 
 const RequestsList=(props) => {
   return (
-    <div  style={{marginTop:"20px", backgroundColor:"ButtonShadow"}}>
     
+    <div  style={{marginTop:"20px", backgroundColor:"ButtonShadow"}}>
+        
         <h5 style={{color:"black"}}>{props.vehicle_details[0].brand}</h5>
         <h5 style={{color:"black"}}>{props.vehicle_details[0].model}</h5>
         <h5 style={{color:"black"}}>{props.vehicle_details[0].color}</h5>
@@ -12,6 +14,7 @@ const RequestsList=(props) => {
         
         
     </div>
+    
   )
 }
 
