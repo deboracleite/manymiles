bfdb4d8a7c8e79c13a5961233872dbfa4747536e
Debora Carvalho Leite Wessen <debora14leite@gmail.com>
2023-11-24 01:24:43 -0500

    updade font

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import RobotoRegular from "../assets/fonts/Roboto/Roboto-Regular.woff";
 
-
 export default createGlobalStyle`
   * {
     margin: 0;
